4dfd2261dcf81bed5bf982a1a4187dfed7c087b3
Van Tabbert <van.tabbert@gmail.com>
2021-07-18 05:17:49 -0400

    Additional webpack and babel configs

---
+++ scripts/build.js
@@ -0,0 +1,11 @@
+
+const fs = require('fs-extra');
+
+function copyPublicFolder() {
+  fs.copySync("public", "build", {
+    dereference: true,
+    filter: file => file !== "public/index.html",
+  });
+}
+
+copyPublicFolder();
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: "development",
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
+    clean: true
   },
   module: {
     rules: [
@@ -20,5 +22,14 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      filename: "index.html",
+      favicon: "public/favicon.ico",
+      title: 'Van Tabbert | Software Engineer',
+      inject: true,
+    }),
+  ],
 };
