2e4c4fe1b12d4d601a539bfdad5784cfef2755b2
Van Tabbert <van.tabbert@gmail.com>
2021-07-18 04:46:10 -0400

    start webpack setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import React from "react"
+import ReactDOM from 'react-dom';
 
 export const App = () => {
-  return (<div>Hello</div>)
+  return <div>Hello</div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
